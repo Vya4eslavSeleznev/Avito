6978d702a39a82fbf59f115c734378a1a0ccd6f6
Vya4eslav111 <vya4eslav.seleznev@gmail.com>
2022-11-17 11:27:32 +0300

    connection to postgre

--- cmd/main.go
+++ cmd/main.go
@@ -5,9 +5,11 @@ import (
 	"Avito/pkg/handler"
 	"Avito/pkg/repository"
 	"Avito/pkg/service"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	"log"
+	"os"
 )
 
 func main() {
@@ -15,22 +17,17 @@ func main() {
 		log.Fatalf("error initializing configs: %s", err.Error())
 	}
 
-	//db, err := repository.NewPostgresDB(repository.Config{
-	//	Host:     viper.GetString("db.host"),
-	//	Port:     viper.GetString("db.port"),
-	//	Username: viper.GetString("db.username"),
-	//	DBName:   viper.GetString("db.dbname"),
-	//	SSLMode:  viper.GetString("db.sslmode"),
-	//	Password: os.Getenv("DB_PASSWORD"),
-	//})
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
 
 	db, err := repository.NewPostgresDB(repository.Config{
-		Host:     "localhost",
-		Port:     "5436",
-		Username: "postgres",
-		DBName:   "avito",
-		Password: "root123",
-		SSLMode:  "disable",
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		Password: os.Getenv("DB_PASSWORD"),
+		SSLMode:  viper.GetString("db.sslmode"),
 	})
 
 	if err != nil {
@@ -43,7 +40,7 @@ func main() {
 
 	srv := new(avito.Server)
 
-	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error occured while running http server: %s", err.Error())
 	}
 }
